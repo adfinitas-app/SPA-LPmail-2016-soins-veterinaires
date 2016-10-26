f766cc1a2e7cde07389b9451850d37ad3def7386
bruere_s <sebastien.bruere@epitech.eu>
2016-10-26 15:03:18 +0200

    OK

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,37 @@ $(window).scroll(function(e){
 
 $( window ).resize(function() {
 });
+
+var p = extractUrlParams();
+
+$('.link_don_45').click( function() {
+	woopra.identify({
+		email: p['email'],
+		name: p['prenom'] + " " + p['lastname'],
+		firstname: p['firstname'],
+		lastname: p['lastname']
+	});
+
+	woopra.track("interaction", {
+		action:'clic',
+		value:'45€'
+		url: document.location.href,
+		title: document.title,
+	});
+});
+
+$('.link_don_45').click( function() {
+	woopra.identify({
+		email: p['email'],
+		name: p['prenom'] + " " + p['lastname'],
+		firstname: p['firstname'],
+		lastname: p['lastname']
+	});
+
+	woopra.track("interaction", {
+		action:'clic',
+		value:'60€'
+		url: document.location.href,
+		title: document.title,
+	});
+});
